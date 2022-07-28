56e0b02926216f9c612beb9f55ce91e18bd86817
João Victor <jovic.marks@gmail.com>
2022-07-28 10:10:55 -0300

    issue fixed

--- src/pages/dashboard.tsx
+++ src/pages/dashboard.tsx
@@ -1,63 +1,45 @@
 import { Box, Flex, SimpleGrid, Text, theme } from "@chakra-ui/react";
+import { ApexOptions } from 'apexcharts';
 import dynamic from "next/dynamic";
 import { Header } from "../components/Header";
 import { Sidebar } from "../components/SideBar";
 
-const Chart = dynamic(() => import('react-apexcharts'), {
-  ssr: false,
-})
+const Chart = dynamic(() => import('react-apexcharts'), { ssr: false });
 
-const options = {
+const options: ApexOptions = {
   chart: {
-    toolbar: {
-      show: false,
-    },
-    zoom: {
-      enabled: false,
-    },
-    foreColor: theme.colors.gray[500],
+    toolbar: { show: false },
+    zoom: { enabled: false },
+    foreColor: theme.colors.gray['500'],
   },
-  grid: {
-    show: false,
-  },
-  dataLabels: {
-    enabled: false,
-  },
-  tooltip: {
-    enabled: false,
-  },
+  grid: { show: false },
+  dataLabels: { enabled: false },
+  tooltip: { enabled: false },
   xaxis: {
     type: 'datetime',
-    axisBorder: {
-      color: theme.colors.gray[600]
-    },
-    axisTicks: {
-      color: theme.colors.gray[600]
-    },
+    axisBorder: { color: theme.colors.gray['600'] },
+    axisTicks: { color: theme.colors.gray['600'] },
     categories: [
-      '2021-03-18T00:00:00.000Z',
-      '2021-03-19T00:00:00.000Z',
-      '2021-03-20T00:00:00.000Z',
-      '2021-03-21T00:00:00.000Z',
-      '2021-03-23T00:00:00.000Z',
-      '2021-03-24T00:00:00.000Z',
-      '2021-03-25T00:00:00.000Z',
-    ]
+      '2021-04-02T00:00:00:00.000Z',
+      '2021-04-03T00:00:00:00.000Z',
+      '2021-04-04T00:00:00:00.000Z',
+      '2021-04-05T00:00:00:00.000Z',
+      '2021-04-06T00:00:00:00.000Z',
+      '2021-04-07T00:00:00:00.000Z',
+    ],
   },
   fill: {
     opacity: 0.3,
     type: 'gradient',
     gradient: {
       shade: 'dark',
-      opacityGrom: '0.7',
-      opacityTo: '0.3'
-    }
-  }
+      opacityFrom: 0.7,
+      opacityTo: 0.3,
+    },
+  },
 };
 
-const series = [
-  { name: 'series1', data: [31, 120, 10, 28, 51, 18, 109] }
-];
+const series = [{ name: 'series1', data: [57, 36, 13, 29, 6, 19] }];
 
 export default function Dashboard() {
   return (
